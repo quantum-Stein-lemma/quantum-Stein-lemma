5f1bb382c3fe23e3bc3d8e8346b374841d2d89f4
quantum-Stein-lemma <105918091+quantum-Stein-lemma@users.noreply.github.com>
2022-05-27 15:04:25 +0200

    Add basics with sorries

---
+++ src/basic.lean
@@ -0,0 +1,29 @@
+import data.complex.basic
+import analysis.inner_product_space.basic
+import linear_algebra.tensor_power
+
+open_locale tensor_product
+
+class complex_Hilbert_space (V : Type) extends inner_product_space ℂ V :=
+(hilbert_ness: 1=1)
+
+variables {ℋ : Type} [complex_Hilbert_space ℋ]
+
+notation ℋ`^⨂`n := ⨂[ℂ]^n ℋ -- set priority
+
+/-- log of operator -/
+def operator.log {n : ℕ} (ρ : (ℋ^⨂n) →ₗ[ℂ] ℋ^⨂n) : (ℋ^⨂n) →ₗ[ℂ] ℋ^⨂n := sorry
+
+/-- trace of operator -/
+def tr {n : ℕ} (ρ : (ℋ^⨂n) →ₗ[ℂ] ℋ^⨂n) : ℝ := sorry
+
+/-- tensor power of operator -/
+def tensor_pow_vec (σ : (ℋ^⨂1) →ₗ[ℂ] ℋ^⨂1) (n : ℕ) : 
+(ℋ^⨂n) →ₗ[ℂ] ℋ^⨂n := sorry
+
+notation σ`^⊗`n := tensor_pow_vec σ n -- set priority
+
+/-- Operators can be less than each other -/
+instance {n : ℕ} : has_le ((ℋ^⨂n) →ₗ[ℂ] ℋ^⨂n) := sorry
+
+def pos_semidef {n : ℕ} (ρ : (ℋ^⨂n) →ₗ[ℂ] ℋ^⨂n) := 1=1
