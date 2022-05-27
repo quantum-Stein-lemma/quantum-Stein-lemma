7adcaa179fdc822ab5481a906d25d46f48cb7323
quantum-Stein-lemma <105918091+quantum-Stein-lemma@users.noreply.github.com>
2022-05-27 10:52:55 +0200

    use order.liminf_infsup from mathlib

--- src/quantum_Stein_lemma.lean
+++ src/quantum_Stein_lemma.lean
@@ -1,45 +1,53 @@
-import POVM
 import Quantum_relative_entropy
 
 import analysis.special_functions.complex.log
+import order.liminf_limsup
+import topology.sequences
 
-open complex
+open real filter
 
 variables
 (ℋ : Type) [complex_Hilbert_space ℋ]
 
 variables
-(A : λ n : ℕ, ℋ^⊗n →ₗ[ℂ] ℋ^⊗n) [∀ n, POVM {A n, 1 - A n}]
+{ρ : ℋ →ₗ[ℂ] ℋ} [quantum_state ρ]
+{σ : ℋ →ₗ[ℂ] ℋ} [quantum_state σ]
 
-variables
-{ρ: ℋ →ₗ[ℂ] ℋ} [quantum_state ρ]
-{σ: ℋ →ₗ[ℂ] ℋ} [quantum_state σ]
+variable {n : ℕ}
 
-variable (n : ℕ)
+-- TODOS: tensor power of vector
 
 /--
-Probability of guessing σ^⊗n when actually ρ^⊗n.
+Hypothesis test
 -/
-def prob_σₙ_when_ρₙ_of_Aₙ := tr(ρ^⊗n * (1 - A n))
+def test (Aₙ : (ℋ^⊗n) →ₗ[ℂ] (ℋ^⊗n)) :=
+0 ≤ Aₙ ∧ Aₙ ≤ 1 
 
 /--
 Probability of guessing ρ^⊗n when actually σ^⊗n.
 -/
-def prob_ρₙ_when_σₙ_of_Aₙ := tr(σ^⊗n * (A n))
+def prob_ρₙ_when_σₙ (Aₙ : (ℋ^⊗n) →ₗ[ℂ] (ℋ^⊗n)) [test Aₙ] :=
+tr(σ^⊗n * Aₙ)
+
+/--
+Probability of guessing σ^⊗n when actually ρ^⊗n.
+-/
+def prob_σₙ_when_ρₙ (Aₙ : (ℋ^⊗n) →ₗ[ℂ] (ℋ^⊗n)) [test Aₙ] :=
+tr(ρ^⊗n * (1 - Aₙ))
 
 /--
 What is the minimum?
 -/
-def min_prob_ρₙ_when_σₙ_if_prob_σₙ_when_ρₙ_leq_ε (ε > 0) :=
-min {prob_ρₙ_when_σₙ_of_Aₙ A n | A : λ n : ℕ, ℋ^⊗n →ₗ[ℂ] ℋ^⊗n ∧ ∀ n, POVM {A n, 1 - A n} ∧ prob_σₙ_when_ρₙ_of_Aₙ A n ≤ ε}
+def min_prob_ρₙ_when_σₙ_if_prob_σₙ_when_ρₙ_leq_ε (ε : ℝ) [ε > 0] :=
+min {prob_ρₙ_when_σₙ Aₙ | Aₙ : (ℋ^⊗n) →ₗ[ℂ] (ℋ^⊗n) ∧ test Aₙ ∧ prob_σₙ_when_ρₙ Aₙ ≤ ε}
 
 /--
 Quantum Stein's lemma.
 -/
-lemma log_minimum_achieves_quantum_relative_entropy : 
-∀ ε > 0, ∀ δ > 0,
-∃ k : ℕ, ∀ n > k,
-1/n * log min_prob_ρₙ_when_σₙ_if_prob_σₙ_when_ρₙ_leq_ε n ε ≥ quantum_relative_entropy ρ σ - δ :=
+lemma log_minimum_tends_to_quantum_relative_entropy : 
+∀ (ε : ℝ) [ε > 0],
+lim at_top (λn:ℕ, log min_prob_ρₙ_when_σₙ_if_prob_σₙ_when_ρₙ_leq_ε n ε / n) = - quantum_relative_entropy ρ σ :=
 begin
-  sorry
+  -- follows from the two lemmas above
+  sorry,
 end
