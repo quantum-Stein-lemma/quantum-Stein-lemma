2538299f0d1ab2d7de07f58d6dcc5f9a66e34a04
quantum-Stein-lemma <105918091+quantum-Stein-lemma@users.noreply.github.com>
2022-05-27 15:03:55 +0200

    add Quantum_state.lean

---
+++ src/Quantum_state.lean
@@ -0,0 +1,11 @@
+import basic
+
+variables
+{ℋ : Type} [complex_Hilbert_space ℋ]
+
+open operator
+
+/-- Quantum state -/
+class quantum_state {n : ℕ} (ρ : (ℋ^⨂n) →ₗ[ℂ] ℋ^⨂n) :=
+(trace_one   : tr ρ = 1)
+(pos_semidef : pos_semidef ρ)
