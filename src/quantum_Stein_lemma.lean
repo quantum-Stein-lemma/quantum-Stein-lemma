8659e1ae00c08b9ceaebb309f27c2f63563dc368
quantum-Stein-lemma <105918091+quantum-Stein-lemma@users.noreply.github.com>
2022-05-29 18:57:45 +0200

    Tidy up

--- src/quantum_Stein_lemma.lean
+++ src/quantum_Stein_lemma.lean
@@ -9,60 +9,41 @@ open real filter
 variables
 {ℋ : Type} [complex_Hilbert_space ℋ]
 
-variables
-{ρ : (ℋ^⨂1) →ₗ[ℂ] ℋ^⨂1} [quantum_state ρ]
-{σ : (ℋ^⨂1) →ₗ[ℂ] (ℋ^⨂1)} [quantum_state σ]
-
-variable {n : ℕ}
-
 /--
 Hypothesis test
 -/
-class test (Aₙ : (ℋ^⨂n) →ₗ[ℂ] ℋ^⨂n) :=
+class test {n : ℕ} (Aₙ : ℋ^⨂n →ₗ[ℂ] ℋ^⨂n) :=
 (geq_zero_leq_one : 0 ≤ Aₙ ∧ Aₙ ≤ 1)
+
+variables 
+(ρ : ℋ^⨂1 →ₗ[ℂ] ℋ^⨂1) [quantum_state ρ]
+(σ : ℋ^⨂1 →ₗ[ℂ] ℋ^⨂1) [quantum_state σ]
 
 /--
 Probability of guessing ρ^⊗n when actually σ^⊗n.
 -/
-noncomputable def prob_ρₙ_when_σₙ (Aₙ : (ℋ^⨂n) →ₗ[ℂ] ℋ^⨂n) [test Aₙ] :=
-tr((σ^⊗n) * Aₙ)
-
-variables (Aₙ : (ℋ^⨂n) →ₗ[ℂ] ℋ^⨂n) [test Aₙ]
-
-#check prob_ρₙ_when_σₙ Aₙ
-
-example : ∃ε:ℝ, prob_ρₙ_when_σₙ Aₙ = ε := sorry
+noncomputable def β {n : ℕ} (Aₙ : ℋ^⨂n →ₗ[ℂ] ℋ^⨂n) :=
+tr(σ^⊗n * Aₙ)
 
 /--
 Probability of guessing σ^⊗n when actually ρ^⊗n.
 -/
-noncomputable def prob_σₙ_when_ρₙ (Aₙ : (ℋ^⨂n) →ₗ[ℂ] ℋ^⨂n) [test Aₙ] :=
-tr((ρ^⊗n) * (1 - Aₙ))
+noncomputable def α {n : ℕ} (Aₙ : ℋ^⨂n →ₗ[ℂ] ℋ^⨂n) :=
+tr(ρ^⊗n * (1 - Aₙ))
 
 /--
 What is the minimum?
 -/
-noncomputable def min_prob_ρₙ_when_σₙ_if_prob_σₙ_when_ρₙ_leq_ε (ε : ℝ) [ε > 0] :=
-Inf {prob : ℝ | ∃ (Aₙ : (ℋ^⨂n) →ₗ[ℂ] ℋ^⨂n) [test Aₙ], (tr((σ^⊗n) * Aₙ) = prob) ∧ (tr((ρ^⊗n) * (1 - Aₙ)) ≤ ε)}
-
--- def min_prob_ρₙ_when_σₙ_if_prob_σₙ_when_ρₙ_leq_ε (ε : ℝ) [ε > 0] :=
--- Inf {prob_ρₙ_when_σₙ Aₙ : ℝ | Aₙ : (ℋ^⊗n) →ₗ[ℂ] (ℋ^⊗n) ∧ test Aₙ ∧ prob_σₙ_when_ρₙ Aₙ ≤ ε}
+noncomputable def βmin (n : ℕ) (ε : ℝ) :=
+Inf {prob : ℝ | ∃ (Aₙ : ℋ^⨂n →ₗ[ℂ] ℋ^⨂n) [test Aₙ], β σ Aₙ = prob ∧ α ρ Aₙ ≤ ε}
 
 /--
 Quantum Stein's lemma.
 -/
-lemma log_min_tends_to_quantum_relative_entropy' : 
+lemma log_inf_tends_to_quantum_relative_entropy  : 
 ∀ (ε : ℝ) [ε > 0],
-lim at_top (λn:ℕ, log(Inf {prob : ℝ | ∃ (Aₙ : (ℋ^⨂n) →ₗ[ℂ] ℋ^⨂n) [test Aₙ], (tr((σ^⊗n) * Aₙ) = prob) ∧ (tr((ρ^⊗n) * (1 - Aₙ)) ≤ ε)})/n) = - quantum_relative_entropy ρ σ :=
+lim at_top (λn:ℕ, log(βmin ρ σ n ε)/n) = - quantum_relative_entropy ρ σ :=
 begin
   -- follows from the two lemmas above
   sorry,
 end
-
--- lemma log_inf_tends_to_quantum_relative_entropy : 
--- ∀ (ε : ℝ) [ε > 0],
--- lim at_top (λn:ℕ, log(Inf_prob_ρₙ_when_σₙ_if_prob_σₙ_when_ρₙ_leq_ε n ε)/n) = - quantum_relative_entropy ρ σ :=
--- begin
---   -- follows from the two lemmas above
---   sorry,
--- end
