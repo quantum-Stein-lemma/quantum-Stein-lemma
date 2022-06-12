127ebb802304d980d3abd1931988cf4036e60bc1
quantum-Stein-lemma <105918091+quantum-Stein-lemma@users.noreply.github.com>
2022-06-12 15:34:07 +0200

    comment out theorem with sorry

--- src/quantum_Stein_lemma.lean
+++ src/quantum_Stein_lemma.lean
@@ -9,9 +9,7 @@ open real filter
 variables
 {ℋ : Type} [complex_Hilbert_space ℋ]
 
-/--
-Hypothesis test
--/
+/-- Hypothesis test -/
 class test {n : ℕ} (Aₙ : ℋ^⨂n →ₗ[ℂ] ℋ^⨂n) :=
 (geq_zero_leq_one : 0 ≤ Aₙ ∧ Aₙ ≤ 1)
 
@@ -19,30 +17,22 @@ variables
 (ρ : ℋ^⨂1 →ₗ[ℂ] ℋ^⨂1) [quantum_state ρ]
 (σ : ℋ^⨂1 →ₗ[ℂ] ℋ^⨂1) [quantum_state σ]
 
-/--
-Probability of guessing ρ^⊗n when actually σ^⊗n.
--/
+/-- Probability of guessing ρ^⊗n when actually σ^⊗n. -/
 noncomputable def β {n : ℕ} (Aₙ : ℋ^⨂n →ₗ[ℂ] ℋ^⨂n) :=
 tr(σ^⊗n * Aₙ)
 
-/--
-Probability of guessing σ^⊗n when actually ρ^⊗n.
--/
+/-- Probability of guessing σ^⊗n when actually ρ^⊗n. -/
 noncomputable def α {n : ℕ} (Aₙ : ℋ^⨂n →ₗ[ℂ] ℋ^⨂n) :=
 tr(ρ^⊗n * (1 - Aₙ))
 
-/--
-What is the minimum?
--/
+/-- What is the minimum? -/
 noncomputable def βmin (n : ℕ) (ε : ℝ) :=
 Inf {prob : ℝ | ∃ (Aₙ : ℋ^⨂n →ₗ[ℂ] ℋ^⨂n) [test Aₙ], β σ Aₙ = prob ∧ α ρ Aₙ ≤ ε}
 
-/--
-Quantum Stein's lemma.
--/
-lemma log_inf_tends_to_quantum_relative_entropy  : 
-∀ (ε : ℝ) [ε > 0],
-lim at_top (λn:ℕ, log(βmin ρ σ n ε)/n) = - quantum_relative_entropy ρ σ :=
-begin
-  sorry,
-end
+/- Quantum Stein's lemma -/
+-- lemma log_inf_tends_to_quantum_relative_entropy  : 
+-- ∀ (ε : ℝ) [ε > 0],
+-- lim at_top (λn:ℕ, log(βmin ρ σ n ε)/n) = - quantum_relative_entropy ρ σ :=
+-- begin
+--   sorry,
+-- end
